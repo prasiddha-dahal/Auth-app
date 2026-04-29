7675e90fa1e8c0f3e9697722963d0d929c3ef446
prasiddha <prasiddhadahal33@gmail.com>
2026-04-29 13:40:31 +0545

    login working

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const authService = require('../services/auth.service')
 
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
     try {
         const user = await authService.register(req.body)
         res.status(201).json({
@@ -15,4 +15,26 @@ const registerUser = async (req,res) => {
     }
 }
 
-module.exports = {registerUser}
+const loginUser = async (req, res) => {
+    try {
+        const { user, token } = await authService.login(req.body)
+        //send token in cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax"
+        })
+
+        res.status(200).json({
+            message: "login successfully",
+            user
+        })
+    }catch (err) {
+        res.status(err.statusCode || 500).json({
+            message: err.message || "something went wrong"
+        })
+    }
+
+}
+
+module.exports = { registerUser, loginUser }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async ({ username, email, password }) => {
     // first check if user exists or not
@@ -26,6 +28,39 @@ const register = async ({ username, email, password }) => {
     }
 }
 
-module.exports = {register}
+const login = async({email, password}) => {
+    // check if user exists or not
+    
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        const err = new Error("Invalid email or password")
+        err.statusCode = 401
+        throw err
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        const err = new Error("Invalid email or password")
+        err.statusCode = 401
+        throw err
+    }
+
+    // if all good now generate token and send it back to controller
+    const token = jwt.sign({userId: user._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    return{
+        user: {
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        },
+        token
+    }
+
+}
+
+module.exports = {register,login}
 
 
